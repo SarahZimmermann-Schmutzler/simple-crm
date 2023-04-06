b435a97524329d8d8ff7ecb98c453f8c3194cff1
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-04-06 14:29:30 +0200

    user model

---
+++ src/models/user.class.ts
@@ -0,0 +1,23 @@
+export class User {
+    firstName: string;
+    lastName: string;
+    birthDate: number;
+    street: string;
+    number: number;
+    zipcode: number;
+    city: string;
+
+    constructor(obj?: any) {
+        this.firstName = obj ? obj.firstName : '';
+        this.lastName = obj ? obj.lastName : '';
+        this.birthDate = obj ? obj.birthDate : '';
+        this.street = obj ? obj.street : '';
+        this.number = obj ? obj.number : '';
+        this.zipcode = obj ? obj.zipcode : '';
+        this.city = obj ? obj.city : '';
+    }
+    // gebe in den construcor ein object vom typen any
+    // ? verhilft dazu, dass wir object optional reingeben
+    // nutzen es für if else abfrage
+    // this.firstName ist der firstName des objects, sofern das object existiert; wenn nicht dann ''
+}
