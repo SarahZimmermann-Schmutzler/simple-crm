940ac002cf667843c2eb2a19657647f9a0a08988
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-04-11 14:16:38 +0200

    disbalen funktioniert

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { Firestore, addDoc, collection, doc, setDoc } from '@angular/fire/firestore';
 import { User } from 'src/models/user.class';
 
@@ -12,15 +12,6 @@ export class DialogAddUserComponent {
   birthDate!: Date;
   coll = collection(this.firestore, 'users')
   loading = false;
-  // @ViewChild('fN') fN!: ElementRef;
-  // @ViewChild('lN') lN!: ElementRef;
-  // @ViewChild('birth') birth!: ElementRef;
-  // @ViewChild('str') str!: ElementRef;
-  // @ViewChild('nr') nr!: ElementRef;
-  // @ViewChild('code') code!: ElementRef;
-  // @ViewChild('ct') ct!: ElementRef;
-  // @ViewChild('cancelbtn') cancelbtn!: ElementRef;
-  // @ViewChild('savebtn') savebtn!: ElementRef;
 
   constructor(public firestore: Firestore) {}
 
@@ -29,16 +20,6 @@ export class DialogAddUserComponent {
   }
 
   saveUser() {
-    // let fN = this.fN.nativeElement;
-    // let lN = this.lN.nativeElement;
-    // let birth = this.birth.nativeElement;
-    // let str = this.str.nativeElement;
-    // let nr = this.nr.nativeElement;
-    // let code = this.code.nativeElement;
-    // let ct = this.ct.nativeElement;
-    // let cancelbtn = this.cancelbtn.nativeElement;
-    // let savebtn = this.savebtn.nativeElement;
-
     this.user.birthDate = this.birthDate.getTime();
     console.log('current user is', this.user);
     this.loading = true;
@@ -52,28 +33,4 @@ export class DialogAddUserComponent {
     });
     // muss mit toJSON() in ein JSON umgewandelt werden, da firebase nur JSONS und keine Objekte speichern kann
   }
-
-  // deactivateFields(fN: any, lN: any, birth: any, str: any, nr: any, code: any, ct: any, cancelbtn: any, savebtn: any) {
-  //   fN.disabled = true;
-  //   lN.disabled = true;
-  //   birth.disabled = true;
-  //   str.disabled = true;
-  //   nr.disabled = true;
-  //   code.disabled = true;
-  //   ct.disabled = true;
-  //   cancelbtn.disabled = true;
-  //   savebtn.disabled = true;
-  // }
-
-  // activateFields(fN: any, lN: any, birth: any, str: any, nr: any, code: any, ct: any, cancelbtn: any, savebtn: any) {
-  //   fN.disabled = false;
-  //   lN.disabled = false;
-  //   birth.disabled = false;
-  //   str.disabled = false;
-  //   nr.disabled = false;
-  //   code.disabled = false;
-  //   ct.disabled = false;
-  //   cancelbtn.disabled = false;
-  //   savebtn.disabled = false;
-  // }
 }
