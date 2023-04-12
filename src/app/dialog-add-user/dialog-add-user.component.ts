236d4a31a9deaac78fac33be1a2d180e50fbd4b6
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-04-12 09:08:46 +0200

    close and cancel function

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Firestore, addDoc, collection, doc, setDoc } from '@angular/fire/firestore';
+import { MatDialogRef } from '@angular/material/dialog';
 import { User } from 'src/models/user.class';
 
 @Component({
@@ -13,10 +14,10 @@ export class DialogAddUserComponent {
   coll = collection(this.firestore, 'users')
   loading = false;
 
-  constructor(public firestore: Firestore) {}
+  constructor(public firestore: Firestore, public dialogRef: MatDialogRef<DialogAddUserComponent>) {}
 
   cancel() {
-    
+    this.dialogRef.close();
   }
 
   saveUser() {
@@ -30,6 +31,7 @@ export class DialogAddUserComponent {
     addDoc(this.coll, this.user.toJSON()).then((result:any) => {
       this.loading = false;
       console.log('Adding user finished', result);
+      this.dialogRef.close();
     });
     // muss mit toJSON() in ein JSON umgewandelt werden, da firebase nur JSONS und keine Objekte speichern kann
   }
