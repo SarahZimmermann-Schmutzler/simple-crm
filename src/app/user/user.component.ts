0e153bf2eb555d84c54ad61bbb8bb80ab6c9f9f0
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-04-12 10:38:30 +0200

    nutzer in tabelle anzeigen

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -3,7 +3,9 @@ import {TooltipPosition} from '@angular/material/tooltip';
 import {FormControl} from '@angular/forms';
 import {MatDialog} from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
-import { User } from 'src/models/user.class';
+import { Firestore, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { collectionData } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-user',
@@ -13,9 +15,20 @@ import { User } from 'src/models/user.class';
 export class UserComponent {
   positionOptions: TooltipPosition[] = ['below', 'above', 'left', 'right'];
   position = new FormControl(this.positionOptions[1]);
+  allUsers$: Observable<any> | undefined
+  allUsers: Array<any> | undefined
   
 
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog, public firestore: Firestore) {
+    const coll = collection(this.firestore, 'users');
+    this.allUsers$ = collectionData(coll);
+    this.allUsers$.subscribe ((newUsers) => {
+      // console.log('Neue User sind:', newUsers);
+      this.allUsers = newUsers;
+    })
+    // update aus Datenbank; collection abonniert
+    // mit dem Array allUsers verknüpft
+  }
 
   openDialog() {
     this.dialog.open(DialogAddUserComponent);
